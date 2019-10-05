cd8ff82f3962e9110674b43c76fb17de7e09c657
Gabe Pataky <gjpataky@gmail.com>
2019-10-04 23:33:00 -0500

    new commit

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /*defined function*/
 console.log("app started");
-var passGen = [];
 var letters = [
   "a",
   "b",
@@ -30,55 +29,58 @@ var letters = [
   "z"
 ];
 
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ["0", "1", "2"];
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 var pwLen = prompt("What is your password length?");
-var pwSymbol = prompt("Would you like to use a symbol?");
-var isLetters = true;
-var isNumbers = true;
-var isSymbols = true;
+var isSymbols = confirm("Would you like to use a symbol?");
+var isNumbers = confirm("Would you like to use a number?");
 
 function shuffle(array) {
-  var currentIndex = array.length;
-  // While there remain elements to shuffle//
-  for (i = 0; i < pwLen; i++) {
-    if (isSymbols === true && i === 0) {
-      var randomLet = Math.floor(Math.random() * symbols.length);
-      passGen.push(symbols[randomLet]);
-    } else {
-      var randomLet = Math.floor(Math.random() * symbols.length);
-      passGen.push(symbols[randomLet]);
-    }
-  }
-  //Pick a remaining element//
-  randomIndex = Math.floor(Math.random() * currentIndex);
-  currentIndex -= 1;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-  //and swap it with the current element//
-  temporaryValue = array[currentIndex];
-  array[currentIndex] = array[randomIndex];
-  array[randomIndex] = temporaryValue;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
   return array;
-
-  /* defined function */
-  function createPassword() {
-    for (i = 0; i < pwLen; i++) {
-      if (isSymbols === true && i === 0) {
-        var randomSymNo = Math.floor(Math.random() * symbols.length);
-        /* inject a symbol into the string*/
-        pw.push(symbols[randomSumNo]);
-      } else {
-        /*inject a letter into the string*/
-        var randomNo = Math.floor(Math.random * letters.length);
-        pw.push(letters[randomNo]);
-      }
-    }
-
-    /*randomize array*/
-    pwShuffle = shuffle(passGen);
-    pwBox.innerHTML = pwShuffle.toString();
+}
+/* defined function */
+function createPassword() {
+  var pw = [];
+  if (pwLen > 10) {
+    pwLen = prompt("Your password has too many characters");
+    createPassword();
   }
+  for (i = 0; i < pwLen; i++) {
+    if (isSymbols === true && i === 0) {
+      var randomSymNo = Math.floor(Math.random() * symbols.length);
+      /* inject a symbol into the string*/
+      pw.push(symbols[randomSymNo]);
+    } else if (isNumbers === true && i === 1) {
+      console.log("in numbers");
+      /*inject a letter into the string*/
+      var randomNo = Math.floor(Math.random() * numbers.length);
+      pw.push(numbers[randomNo]);
+    } else {
+      console.log("in letters");
+
+      var randomLet = Math.floor(Math.random() * letters.length);
+      pw.push(letters[randomLet]);
+    }
+  }
+  /*randomize array*/
+  pwShuffle = shuffle(pw);
+  pwBox.innerHTML = pwShuffle.toString();
 }
 
 /* first event */
